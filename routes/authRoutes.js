38f9c77d714050362d8eed629c70551cc433ac0d
Vakar Younish <younishwaqar@gmail.com>
2026-03-17 21:42:12 +0530

    first commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const { register, login } = require("../controllers/authController");
+
+router.post("/register", register);
+router.post("/login", login);
+
+module.exports = router;
+
+const protect = require("../middleware/authMiddleware");
+
+router.get("/profile", protect, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user
+  });
+});
